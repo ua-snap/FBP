9df8046cc2e5c5b819c9b494f543f95c615049c6
Bob Torgerson <rltorgerson@alaska.edu>
2021-04-09 15:24:03 -0800

    Updated tests to include one that was broken before.

--- Tests/FBPTests/FBPTests.swift
+++ Tests/FBPTests/FBPTests.swift
@@ -48,9 +48,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 1.116338263490523)
         XCTAssertEqual(secs.lbt,1.1162210194837365)
-        XCTAssertEqual(secs.area, 5.466572998503012)
-        XCTAssertEqual(secs.perm, 830.7030107277245)
-        XCTAssertEqual(secs.pgr, 16.1725886677322)
+        XCTAssertEqual(secs.area, 5.46657290525333)
+        XCTAssertEqual(secs.perm, 830.7029965574588)
+        XCTAssertEqual(secs.pgr, 16.17258839185758)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 4.540518390451227)
@@ -129,8 +129,8 @@ final class FBPTests: XCTestCase {
         XCTAssertEqual(mains.sfi, 2454.630189578547)
         XCTAssertEqual(mains.sf, 1.249717130124279)
         XCTAssertEqual(mains.isi, 11.80884578388611)
-        XCTAssertEqual(mains.raz, 180)
-        XCTAssertEqual(mains.wsv, 6.161144702827245)
+        XCTAssertEqual(mains.raz, 180.00000011291033)
+        XCTAssertEqual(mains.wsv, 6.161144702827244)
         XCTAssertEqual(mains.ff, 41.62104842399954)
         XCTAssertEqual(mains.jd, 83)
         XCTAssertEqual(mains.jd_min, 103)
@@ -139,9 +139,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 1.1886771678129722)
         XCTAssertEqual(secs.lbt, 1.1884870217124561)
-        XCTAssertEqual(secs.area, 6.834943667308616)
-        XCTAssertEqual(secs.perm, 931.9525286677097)
-        XCTAssertEqual(secs.pgr, 18.14377064890147)
+        XCTAssertEqual(secs.area, 6.834943550717048)
+        XCTAssertEqual(secs.perm, 931.9525127703135)
+        XCTAssertEqual(secs.pgr, 18.143770339402113)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 5.567918297096819)
@@ -228,9 +228,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 1.116338263490523)
         XCTAssertEqual(secs.lbt, 1.1046743237122443)
-        XCTAssertEqual(secs.area, 0.31102504521793733)
-        XCTAssertEqual(secs.perm, 198.06551307359243)
-        XCTAssertEqual(secs.pgr, 14.635783143445767)
+        XCTAssertEqual(secs.area, 0.3110250399124221)
+        XCTAssertEqual(secs.perm, 198.06550969495902)
+        XCTAssertEqual(secs.pgr, 14.635782893786224)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 4.540518390451227)
@@ -318,9 +318,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 1.116338263490523)
         XCTAssertEqual(secs.lbt,1.116338263490403)
-        XCTAssertEqual(secs.area, 111.06582208347926)
-        XCTAssertEqual(secs.perm, 3744.383334850105)
-        XCTAssertEqual(secs.pgr, 16.188123440124105)
+        XCTAssertEqual(secs.area, 111.0658201889006)
+        XCTAssertEqual(secs.perm, 3744.3832709778117)
+        XCTAssertEqual(secs.pgr, 16.188123163984493)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 4.540518390451227)
@@ -408,9 +408,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 1.116338263490523)
         XCTAssertEqual(secs.lbt, 1.1162210194837365)
-        XCTAssertEqual(secs.area, 4.069220075382254)
-        XCTAssertEqual(secs.perm, 716.7105506741715)
-        XCTAssertEqual(secs.pgr, 13.953319995461483)
+        XCTAssertEqual(secs.area, 4.069220005968843)
+        XCTAssertEqual(secs.perm, 716.7105384484076)
+        XCTAssertEqual(secs.pgr, 13.953319757443504)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 3.1338874426931334)
@@ -500,9 +500,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 1.116338263490523)
         XCTAssertEqual(secs.lbt, 1.1162210194837365)
-        XCTAssertEqual(secs.area, 8.828159700381056)
-        XCTAssertEqual(secs.perm, 1055.6581355604449)
-        XCTAssertEqual(secs.pgr, 20.552140271175688)
+        XCTAssertEqual(secs.area, 8.828159549788886)
+        XCTAssertEqual(secs.perm, 1055.6581175528584)
+        XCTAssertEqual(secs.pgr, 20.55213992059397)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 4.581280251176898)
@@ -592,9 +592,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 1.116338263490523)
         XCTAssertEqual(secs.lbt, 1.116166467854431)
-        XCTAssertEqual(secs.area, 401.36502221615143)
-        XCTAssertEqual(secs.perm, 7117.985463447058)
-        XCTAssertEqual(secs.pgr, 139.88820215908893)
+        XCTAssertEqual(secs.area, 401.3650153696023)
+        XCTAssertEqual(secs.perm, 7117.985342027316)
+        XCTAssertEqual(secs.pgr, 139.88819977285345)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 29.61406232507023)
@@ -685,9 +685,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 2.283818198160697)
         XCTAssertEqual(secs.lbt, 2.282524384887043)
-        XCTAssertEqual(secs.area, 0.0350418822094813)
-        XCTAssertEqual(secs.perm, 74.84019593062051)
-        XCTAssertEqual(secs.pgr, 1.457030598141319)
+        XCTAssertEqual(secs.area, 0.03504188161173123)
+        XCTAssertEqual(secs.perm, 74.84019465398441)
+        XCTAssertEqual(secs.pgr, 1.4570305732870565)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 0.4005621595072791)
@@ -775,9 +775,9 @@ final class FBPTests: XCTestCase {
         // Secondary Output Assertions
         XCTAssertEqual(secs.lb, 1.116338263490523)
         XCTAssertEqual(secs.lbt, 1.1162210194837365)
-        XCTAssertEqual(secs.area, 157.72689162103038)
-        XCTAssertEqual(secs.perm, 4462.118307244551)
-        XCTAssertEqual(secs.pgr, 86.87100328022824)
+        XCTAssertEqual(secs.area, 157.72688893049968)
+        XCTAssertEqual(secs.perm, 4462.118231129019)
+        XCTAssertEqual(secs.pgr, 86.87100179836868)
         
         // Head Fire Output Assertions
         XCTAssertEqual(heads.ros, 18.53006966637588)
@@ -817,6 +817,100 @@ final class FBPTests: XCTestCase {
         XCTAssertEqual(backs.fd, "S")
         
     }
+    
+    func test_M1_FMC() {
+        
+        /*
+         This tests S1 jack or lodgepole pine slash forest fire.
+        */
+        
+        let fbp = FBPAlgorithm()
+        let input = InputSet()
+        input.fueltype = "M1"
+        input.ffmc = 97.0
+        input.ws = fbp.conversions(6.0, "mi2km")
+        input.bui = 120.0
+        input.pc = 15.0
+        input.fmc = 5.0
+        input.lat = 64.8378
+        input.lon = -147.7164
+        input.wdir = 45
+        input.time = 60.0
+        input.mon = 3.0
+        input.jd = 99.0
+        input.ps = 10.0
+        input.saz = 180.0
+        
+        let mains = MainOutput()
+        let secs = SecondaryOutput()
+        let heads = FireOutput()
+        let flanks = FireOutput()
+        let backs = FireOutput()
+        
+        fbp.sequence_calculate(input, mains, secs, heads, flanks, backs)
+      
+        
+        // Main Output Assertions
+        XCTAssertEqual(mains.sfc, 1.6944758304849126)
+        XCTAssertEqual(mains.csi, 210.3548790805671)
+        XCTAssertEqual(mains.rso, 0.4138052120034652)
+        XCTAssertEqual(mains.rss, 11.909719448582615)
+        XCTAssertEqual(mains.fmc, 5.0)
+        XCTAssertEqual(mains.sfi, 6054.219526043803)
+        XCTAssertEqual(mains.sf, 1.249717130124279)
+        XCTAssertEqual(mains.raz, 214.11722357679082)
+        XCTAssertEqual(mains.isi, 20.95681908753616)
+        XCTAssertEqual(mains.wsv, 12.173288824737837)
+        XCTAssertEqual(mains.ff, 54.55826767723967)
+        XCTAssertEqual(mains.jd, 99.0)
+        XCTAssertEqual(mains.jd_min, 0)
+        XCTAssertEqual(mains.covertype, "c")
+        
+        // Secondary Output Assertions
+        XCTAssertEqual(secs.lb, 1.680008679635945)
+        XCTAssertEqual(secs.lbt, 1.6788140901936326)
+        XCTAssertEqual(secs.area, 24.290918563307613)
+        XCTAssertEqual(secs.perm, 1835.0780541415877)
+        XCTAssertEqual(secs.pgr, 36.231790246973915)
+        
+        // Head Fire Output Assertions
+        XCTAssertEqual(heads.ros, 11.909719448582615)
+        XCTAssertEqual(heads.dist, 602.1472003985743)
+        XCTAssertEqual(heads.rost, 11.888797325762367)
+        XCTAssertEqual(heads.rss, 11.909719448582615)
+        XCTAssertEqual(heads.cfb, 0.9289278893509791)
+        XCTAssertEqual(heads.fi, 6452.497265848633)
+        XCTAssertEqual(heads.fc, 1.8059471772070301)
+        XCTAssertEqual(heads.cfc, 0.1114713467221175)
+        XCTAssertEqual(heads.time, 0)
+        XCTAssertEqual(heads.fd, "C")
+        
+        // Flank Fire Output Assertions
+        XCTAssertEqual(flanks.ros, 4.241658238029177)
+        XCTAssertEqual(flanks.dist, 214.607914181383)
+        XCTAssertEqual(flanks.rost, 4.237219727199958)
+        XCTAssertEqual(flanks.rss, 4.241658238029177)
+        XCTAssertEqual(flanks.cfb, 0.5853855372582116)
+        XCTAssertEqual(flanks.fi, 2245.6044035705377)
+        XCTAssertEqual(flanks.fc, 1.764722094955898)
+        XCTAssertEqual(flanks.cfc, 0.07024626447098539)
+        XCTAssertEqual(flanks.time, 0)
+        XCTAssertEqual(flanks.fd, "I")
+        
+        // Back Fire Output Assertions
+        XCTAssertEqual(backs.ros, 2.342325863294038)
+        XCTAssertEqual(backs.dist, 118.426379990969)
+        XCTAssertEqual(backs.rost, 2.3382110367770528)
+        XCTAssertEqual(backs.rss, 2.342325863294038)
+        XCTAssertEqual(backs.cfb, 0.3582521117172205)
+        XCTAssertEqual(backs.fi, 1220.9135234682146)
+        XCTAssertEqual(backs.fc, 1.737466083890979)
+        XCTAssertEqual(backs.cfc, 0.04299025340606646)
+        XCTAssertEqual(backs.isi, 6.145007964355635)
+        XCTAssertEqual(backs.time, 0)
+        XCTAssertEqual(backs.fd, "I")
+        
+    }
 
     static var allTests = [
         ("C1", test_C1), ("C1 with 10% slope N", test_C1_with_slope),
@@ -824,6 +918,7 @@ final class FBPTests: XCTestCase {
         ("C1 over 240 minutes", test_C1_over_240_min),
         ("D1", test_D1),
         ("M1 with 10% conifer", test_M1),
+        ("M1 with 15% conifer, 5% FMC and 10% slope", test_M1_FMC),
         ("M3 with 60% dead fir", test_M3),
         ("O1a with 20% cured grass & 100 grass fuel load", test_O1a)
     ]
